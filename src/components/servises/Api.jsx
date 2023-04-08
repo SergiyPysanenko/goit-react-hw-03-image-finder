2112d8d58b60414801e92a2dd231896b99f9874b
SergiyPysanenko <SergiyPysanenko@users.noreply.github.com>
2023-04-08 20:38:20 +0300

    add task

--- src/components/servises/Api.jsx
+++ src/components/servises/Api.jsx
@@ -1,14 +1,22 @@
-const BASE_URL = 'https://pixabay.com/api';
+
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 const API_KEY = '34993491-75f64346bf709c0012e47f9c8';
+const BASE_FILTERS = 'image_type=photo&orientation=horizontal&per_page=12';
 
-function fetchQuery(searchQuery, page) {
-  return fetch(
-    `${BASE_URL}/?key=${API_KEY}&q=${searchQuery}
-      &image_type=photo&orientation=horizontal&page=
-      ${page}&per_page=12`
-  ).then(response => response.json());
-}
-
-const api = { fetchQuery };
-
-export default api;
+export const getImagesAPI = async (query, page) => {
+  const response = await axios.get(
+    `?q=${query}&page=${page}&key=${API_KEY}&${BASE_FILTERS}`
+  );
+  const images = response.data.hits.map(img => {
+    const { id, largeImageURL, webformatURL, tags } = img;
+    return {
+      id,
+      largeImageURL,
+      webformatURL,
+      tags,
+    };
+  });
+  return images;
+};
